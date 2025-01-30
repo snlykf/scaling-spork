b33fe43833f11d15f0570d3ffe833c56147af6dd
John Doe <john.doe@example.com>
2025-01-30 16:31:08 +0100

    Add billingRepo.getInvoice

--- src/repos/billingRepo.js
+++ src/repos/billingRepo.js
@@ -4,8 +4,27 @@ const mockPlans = [
     'Enterprise'
 ];
 
+const mockInvoices = {
+    'jdoe': {
+        invoiceNr: 1,
+        date: 'July 2024',
+        amount: 20.37
+    }
+}
+
 const getPlans = () => Promise.resolve(mockPlans);
 
+const getInvoice = (user) => {
+    let invoice = mockInvoices[user.username];
+
+    if(invoice) {
+        return Promise.resolve(invoice);
+    } else {
+        return Promise.reject(`No invoice for ${JSON.stringify(user)}`);
+    }
+}
+
 export {
-    getPlans
+    getPlans,
+    getInvoice
 };
